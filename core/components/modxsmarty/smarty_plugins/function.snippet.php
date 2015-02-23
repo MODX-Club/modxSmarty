a9fa373c765408532a411dc538aa754dce662ccd
Fi1osof <info@newpg.ru>
2015-02-23 17:37:06 +0400

    1.0.3

--- core/components/modxsmarty/smarty_plugins/function.snippet.php
+++ core/components/modxsmarty/smarty_plugins/function.snippet.php
@@ -9,30 +9,42 @@
 
 function smarty_function_snippet($params, & $smarty)
 {
-    if(!isset($params['name']) OR !$name = $params['name']){return;}
+    if(!isset($params['name']) OR !$name = $params['name']) return;
+    
     if(!empty($params['assign'])){
         $assign = (string)$params['assign'];
     }
-
+    
     $modx = & $smarty->modx;
     $modx->getParser();
-    $scriptProperties = array();
 
-    if(isset($params['params'])){
-        $scriptProperties = $params['params'];
-        // Check if String
-        if(is_string($scriptProperties)){
-            $scriptProperties = $modx->parser->parseProperties($scriptProperties);
+    $as_tag = !empty($params['as_tag']);
+    
+    $scriptProperties = !empty($params['params']) ? $params['params'] : array();
+    if($scriptProperties && !is_array($scriptProperties)){
+        $scriptProperties = $modx->parser->parseProperties($scriptProperties);
+    }
+    
+    
+    // Output as MODX-tag like [[!$name...]]
+    if($as_tag){
+        $tag = new modFieldTag($modx);
+        $tag->_token = '!';
+        $tag->name= $name;
+        $tag->_properties = $scriptProperties;
+        $output = $tag->getTag();
+    }
+    
+    // execute via MODX API
+    else{
+        $output = $modx->runSnippet($name, $scriptProperties);
+        if(isset($params['parse']) && $params['parse'] == 'true'){
+            $maxIterations= intval($modx->getOption('parser_max_iterations', $params, 10));
+            $modx->parser->processElementTags('', $output, true, false, '[[', ']]', array(), $maxIterations);
+            $modx->parser->processElementTags('', $output, true, true, '[[', ']]', array(), $maxIterations);
         }
     }
-
-    $output = $modx->runSnippet($name, $scriptProperties);
-
-    if(isset($params['parse']) && $params['parse'] == 'true'){
-        $maxIterations= intval($modx->getOption('parser_max_iterations', $params, 10));
-        $modx->parser->processElementTags('', $output, true, false, '[[', ']]', array(), $maxIterations);
-        $modx->parser->processElementTags('', $output, true, true, '[[', ']]', array(), $maxIterations);
-    }
+    
     
     if(!empty($assign)){
         $smarty->assign($assign, $output);
