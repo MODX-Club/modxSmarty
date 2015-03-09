b1b0cdd3744f37b080853afba4af3dfd2cd9e168
Nikolay Lanets <info@newpg.ru>
2015-03-09 04:13:09 +0300

    Added replacing whitespaces

--- core/components/modxsmarty/elements/snippets/smarty.php
+++ core/components/modxsmarty/elements/snippets/smarty.php
@@ -3,4 +3,4 @@ foreach($scriptProperties as $key => $value){
     $modx->smarty->assign($key, $value);
 }
 
-return $modx->smarty->fetch($tpl);
+return preg_replace("/[ \r\n\t]+$/sm", "", $modx->smarty->fetch($tpl));
