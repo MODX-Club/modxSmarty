a2a2e1e11eca11f9b2e8d915c836020f86f4148a
Tramp1357 <maa1972@yandex.ru>
2015-06-17 14:56:53 +0300

    Update function.lessphp.php

--- core/components/modxsmarty/smarty_plugins/function.lessphp.php
+++ core/components/modxsmarty/smarty_plugins/function.lessphp.php
@@ -20,15 +20,22 @@ function smarty_function_lessphp($params, & $smarty)
 {
     if(!isset($params['file']) OR !$file = $params['file'])
     	return;
-	
+
 	$modx=$smarty->modx;
-	$template = $modx->getOption('modxSmarty.template', array(), 'default');
-	$template_url = $modx->getOption('modxSite.template_url').$template.'/';
-	$template_dir = MODX_ASSETS_PATH.'components/modxsite/templates/'.$template.'/';
+
+	$template_dir = MODX_ASSETS_PATH.'components/modxsite/templates/';
+    $template = $modx->getOption('modxSmarty.template', $scriptProperties, 'default');
+    $template_url=$modx->getOption('modxSite.template_url');
+    if($pre_template = $modx->getOption('modxSmarty.pre_template', null, false)){
+        $template=$pre_template;
+    }
+	$template_url .= $template.'/';
+    $template_dir .= $template .'/';
+
 	$less_fname=$template_dir.$file;
-	
+
 	$less_files = array( $less_fname => $template_url.'css/' );
-	$options = array( 'cache_dir' => $template_dir.'cache/' , 'compress'=>true);
+	$options = array( 'cache_dir' => $template_dir.'/cache/' , 'compress'=>true);
 	return $template_url.'cache/'.Less_Cache::Get( $less_files, $options );
 }
 
